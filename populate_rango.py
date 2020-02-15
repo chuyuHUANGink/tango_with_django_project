be15b79aa4fd0201dfb03c0ac2bded43dcd76381
chuyu  HUANG <2431017h@student.gla.ac.uk>
2020-02-15 07:39:06 +0000

    commit after chapter5

---
+++ populate_rango.py
@@ -0,0 +1,74 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup() #imports Django projects settings into the context of this script
+
+from rango.models import Category, Page
+
+def populate():
+
+    python_cat = add_cat('Python', views=128, likes=64)
+
+    add_page(cat=python_cat,
+        title="Official Python Tutorial",
+        url="http://docs.python.org/2/tutorial/")
+
+    add_page(cat=python_cat,
+        title="How to Think like a Computer Scientist",
+        url="http://www.greenteapress.com/thinkpython/")
+
+    add_page(cat=python_cat,
+        title="Learn Python in 10 Minutes",
+        url="http://www.korokithakis.net/tutorials/python/")
+
+    django_cat = add_cat("Django", views=64, likes=32)
+
+    add_page(cat=django_cat,
+        title="Official Django Tutorial",
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+
+    add_page(cat=django_cat,
+        title="Django Rocks",
+        url="http://www.djangorocks.com/")
+
+    add_page(cat=django_cat,
+        title="How to Tango with Django",
+        url="http://www.tangowithdjango.com/")
+
+    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
+
+    add_page(cat=frame_cat,
+        title="Bottle",
+        url="http://bottlepy.org/docs/dev/")
+
+    add_page(cat=frame_cat,
+        title="Flask",
+        url="http://flask.pocoo.org")
+
+
+
+    cats = {
+        "Python": {"pages": python_pages},
+        "Django": {"pages": django_pages},
+        "Other Frameworks": {"pages": other_pages}
+    }
+
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat)
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"])
+
+    # Print out the categories we have added.    
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
+
+def add_page(cat, title, url,views):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url = url
+    p.views = views
+    p.save()
