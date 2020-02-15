a5a089cf26ab98adfb8caa542281d4bce18deca8
chuyu  HUANG <2431017h@student.gla.ac.uk>
2020-02-15 07:41:46 +0000

    commit after chapter9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+LOGIN_URL = '/rango/login/'
+
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
